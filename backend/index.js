58c331d7547fef91e42d8026eceedb4d829a6b20
Satyam Maurya <76580910+satyam4321@users.noreply.github.com>
2023-09-01 16:41:22 +0530

    Update index.js
    Cors changes

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,14 @@ var PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
